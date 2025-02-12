e7c3fe326d6b2083cca1d27fa4aa1907cb411794
romano <romanong321@gmail.com>
2025-02-11 23:53:39 -0400

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }));
+  app.enableCors({
+    origin: '*', // Permite cualquier origen. Cambia '*' por una URL específica para mayor seguridad.
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Permite el envío de cookies en las solicitudes
+  });
+
   await app.listen(3000);
 }
 bootstrap();
